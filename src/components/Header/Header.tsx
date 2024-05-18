a4302a37f52e26455c0a5452dda5eb9fc342d9fb
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2024-05-18 23:47:23 +0200

    Header and CSS adjustments

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,12 @@
 import styles from "./Header.module.css";
 
 export default function Header() {
-  return <div className={styles.header}>Simple Scale Finder</div>;
+  return (
+    <div className={styles.header}>
+      <span>Simple Scale Finder</span>
+      <span className={styles.credits}>
+        , by <span className={styles.gradient}>eflisback</span>
+      </span>
+    </div>
+  );
 }
