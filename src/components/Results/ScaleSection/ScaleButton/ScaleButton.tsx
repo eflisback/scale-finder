26bb81d15196dae1e98100d78ca24cc3c729d7a0
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2024-05-22 20:24:10 +0200

    Buttons for consistency

--- src/components/Results/ScaleSection/ScaleButton/ScaleButton.tsx
+++ src/components/Results/ScaleSection/ScaleButton/ScaleButton.tsx
@@ -20,7 +20,10 @@ export default function ScaleButton({ scale }: IProps) {
         setIsOpen={setIsPianoModalOpen}
         scale={scale}
       />
-      <div className={styles.scale} onClick={() => setIsPianoModalOpen(true)}>
+      <button
+        className={styles.scale}
+        onClick={() => setIsPianoModalOpen(true)}
+      >
         {scale.annotations.length > 1 ? (
           <span>
             {scale.annotations[0]}, {scale.annotations[2]}
@@ -29,7 +32,7 @@ export default function ScaleButton({ scale }: IProps) {
           <span>{scale.annotations[0]}</span>
         )}{" "}
         {scale.type.name}
-      </div>
+      </button>
     </>
   );
 }
